0fe59a0d99845d9a63e882b95b37cf23a7536eb4
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-25 10:46:51 +0200

    put

--- src/server.js
+++ src/server.js
@@ -1,26 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import listEndpoints from 'express-list-endpoints';
-import reviewsRouter from './services/reviews/reviews.js'
+import reviewsRouter from './services/reviews/reviews.js';
+import productsRouter from './services/products/products.js';
 
 const server = express();
-
 
 // const publicFolderPath = join(process.cwd(), "./public")
 
 // server.use(express.static())
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
 
+server.use("/products" , productsRouter)
+server.use('/reviews', reviewsRouter);
 
+const port = 3001;
 
-// server.use("/products" , productsRouter)
-server.use("/reviews" , reviewsRouter)
+console.table(listEndpoints(server));
 
-
-
-const port = 3001
-
-console.table(listEndpoints(server))
-
-server.listen(port)
+server.listen(port);
--- src/services/products/products.js
+++ src/services/products/products.js
@@ -1,30 +1,52 @@
-import fs from "fs-extra"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
-import express from "express";
-import uniqid from "uniqid";
-import multer from "multer";
-import { extname } from "path";
-import { validationResult } from "express-validator";
-import createHttpError from "http-errors";
+import fs from 'fs-extra';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import express from 'express';
+import uniqid from 'uniqid';
+import multer from 'multer';
+import { extname } from 'path';
+import { validationResult } from 'express-validator';
+import createHttpError from 'http-errors';
 
-const dataFolder = join(dirname(fileURLToPath(import.meta.url)), "../../data")
+const { readJSON, writeJSON, writeFile } = fs;
 
+const productsRouter = express.Router();
 
-const productsRouter = express.Router()
+// const dataFolder = join(dirname(fileURLToPath(import.meta.url)), '../../data/products.json');
 
-
-productsRouter.get()
-
-
-productsRouter.delete()
+const allProducts = () => readJSON(dataFolder);
 
 
-productsRouter.post()
+productsRouter.get('/', async (req, res, next) => {
+	try {
+		const products = await allProducts();
+		res.send(products);
+	} catch (error) {
+		next(error);
+	}
+});
+productsRouter.get('/:id', async (req, res, next) => {
+	try {
+		const products = await allProducts();
+		const singleProduct = products.filter((pro) => pro.id === req.params.id);
+		res.send(singleProduct);
+	} catch (error) {
+		next(error);
+	}
+});
 
+productsRouter.delete('/:id', async (req, res, next) => {
+	try {
+		const products = await allProducts();
+		const deletedProduct = products.filter((pro) => pro.id !== req.params.id);
+		res.send(deletedProduct);
+	} catch (error) {
+		next(error);
+	}
+});
 
-productsRouter.put()
+// productsRouter.post();
 
+// productsRouter.put();
 
-
-export default productsRouter
+export default productsRouter;
